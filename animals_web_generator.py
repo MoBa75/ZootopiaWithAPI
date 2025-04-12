f61df76dfe8b28a4b6b450640eb3858321ad14c5
MoBa75 <barison@gmx.de>
2025-04-12 23:13:31 +0200

    added user search request, removed skin types, added fallback if no animal is found

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,7 +1,8 @@
-import json
 import html_operations as op
 import requests
 
+URL = 'https://api.api-ninjas.com/v1/animals?name={}'
+API_KEY = 'VXnBndct7foyIyNdu7byRJDwThtk80OiM5AWCzzs'
 
 def request_data(animal_name):
     """
@@ -9,16 +10,11 @@ def request_data(animal_name):
     :param file_path:
     :return: data entries as dictionary
     """
-
-    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
-    response = requests.get(api_url,
-                            headers={'X-Api-Key': 'VXnBndct7foyIyNdu7byRJDwThtk80OiM5AWCzzs'})
+    api_url = URL.format(animal_name)
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     res = response.json()
-    if response.status_code == requests.codes.ok:
-        print(response.status_code)
-        #print(f"Animal name: {res[0]['name']}, location: {res[0]['locations'][0]}")
-    else:
-        print("Error:", response.status_code, res)
+    if response.status_code != requests.codes.ok:
+        print("Error:", response.status_code)
     return res
 
 
@@ -28,7 +24,6 @@ def get_animals_info(animals_data):
     :param animals_data: overall database as dictionary
     :return: desired data as dictionary
     """
-    # extrahiert Tier Daten aus den Gesamtdaten
     animal_info = {}
     for animal in animals_data:
         for category, detail in animal.items():
@@ -49,7 +44,7 @@ def get_animals_info(animals_data):
     return animal_info
 
 
-def connect_animal_info(animal_info, user_input):
+def connect_animal_info(animal_info, animal_name):
     """
     Selects corresponding data entries according to user
     selection and creates the data for later output
@@ -61,58 +56,28 @@ def connect_animal_info(animal_info, user_input):
     # Filter und Auswahl des User wird hier erstellt.
     complete_animals_info = ''
     for animal, infos in animal_info.items():
-        if user_input.capitalize() in infos.get("Skin Type", "") or user_input == 'all':
-            complete_animals_info += op.serialize_animal(animal, infos)
+        complete_animals_info += op.serialize_animal(animal, infos)
+    if not complete_animals_info:
+        user_info = ("The animal you are looking for, "
+                     "\nis not in the animal list! "
+                     "\nPlease try again.")
+        return (f'<li class="cards__item">'
+                f'\n  <div class="card__title">{animal_name}</div>'
+                f'\n<div class="card__text">'
+                f' \n<h3>{user_info}</h3></div>')
     # corrects the formatting error happening for ' symbol
     return complete_animals_info.replace("’", "'")
-
-
-def get_skin_types(animal_info):
-    """
-    Extracts all skin types from the database.
-    :param animal_info: database with animal info as dictionary
-    :return: all skin types as list
-    """
-    skin_types = []
-    for animal, characteristics in animal_info.items():
-        if characteristics['Skin Type'] in skin_types:
-            continue
-        skin_types.append(characteristics['Skin Type'])
-    return skin_types
-
-
-def get_user_input(skin_types):
-    """
-    User query whether database should be filtered by
-    skin type and selection of skin type
-    :param skin_types: for selection by user as list
-    :return: user selection as string
-    """
-    print("Would you like to filter the animals by their skin type? ")
-    while True:
-        user_input = input("Please enter 'y' for yes or 'n' for no: ")
-        if user_input.lower() == 'n':
-            return 'all'
-        if user_input.lower() == 'y':
-            print('Which skin type do you want to choose?')
-            while True:
-                user_input = input(f'Please enter one of these skin types: '
-                                   f'"{", ".join(skin_types)}": ')
-                if user_input.capitalize() in skin_types:
-                    return user_input
-                print(f"ERROR! '{user_input}' is not a valid skin type!")
-        print(f"ERROR! '{user_input}' is not a valid answer!")
 
 
 def main():
     """Calls all necessary functions and transfers
     the necessary data to execute the program. """
-    animal_name ='fox'
+    animal_name = input('Please enter a name of an animal: ')
     animals_data = request_data(animal_name)
     animal_info = get_animals_info(animals_data)
-    skin_types = get_skin_types(animal_info)
-    user_input = get_user_input(skin_types)
-    animal_str = connect_animal_info(animal_info, user_input)
+
+
+    animal_str = connect_animal_info(animal_info, animal_name)
     html_data = op.read_html_file('animals_template.html')
     chanced_html = op.chance_html_content(html_data, animal_str)
     op.write_html_file(chanced_html)
